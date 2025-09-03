879ef386472a9e08f0ba06ab7bd646b7336a6348
lishihang <lishihang@kilox.com>
2025-09-03 11:05:04 +0800

    progress update

--- 01base/2matrix_transform/matrix_transform.cpp
+++ 01base/2matrix_transform/matrix_transform.cpp
@@ -8,7 +8,7 @@
 #include <pcl/visualization/pcl_visualizer.h>
 
 
-/* 1. show help 函数*/
+/* show help 函数*/
 void showHelp(char *program_name)
 {
     std::cout << std::endl;     // 空行
@@ -20,38 +20,34 @@ void showHelp(char *program_name)
 // This is the main function
 int main(int argc, char **argv)
 {
-
-    // Show help 展示帮助信息。pcl::console::find_switch是判断argv命令行中是否有argument_name，返回bool值
+    /* 1. 检测命令行中是否有'-h'或'--help'参数，以触发showHelp函数 */
     if (pcl::console::find_switch(argc, argv, "-h") || pcl::console::find_switch(argc, argv, "--help"))
     {
         showHelp(argv[0]);
         return 0;
     }
 
-    // 从命令行参数中提取一个 .ply 或 .pcd 文件名。
-    std::vector<int> filenames;
+    /* 2. 从命令行参数中提取一个 .ply 或 .pcd 文件名。*/
+    std::vector<int> filenames;     // 用来存储文件名参数
     bool file_is_pcd = false;       // 默认提取ply文件，如果为pcd文件需要标志位标识
-
     filenames = pcl::console::parse_file_extension_argument(argc, argv, ".ply");        // 获取argv中所有ply文件
-
-    if (filenames.size() != 1)      // ply文件需要一个，不是一个就去找pcd文件
+    if (filenames.size() != 1)          // ply文件需要一个，不是一个就去找pcd文件
     {
         filenames = pcl::console::parse_file_extension_argument(argc, argv, ".pcd");    // 获取argv中所有pcd文件
 
-        if (filenames.size() != 1)  // pcd文件需要一个，不是一个就报错
+        if (filenames.size() != 1)      // pcd文件需要一个，不是一个就报错
         {
-            showHelp(argv[0]);      // argv[0]为可执行程序名称
-            return -1;              // 程序退出
+            showHelp(argv[0]);          // argv[0]为可执行程序名称
+            return -1;                  // 程序退出
         }
         else
         {
-            file_is_pcd = true;     // 若确定为pcd文件，则pcd文件标志位标识
+            file_is_pcd = true;         // 若确定为pcd文件，则pcd文件标志位标识
         }
     }
 
-    // 加载ply/pcd文件
+    /* 3. 加载ply/pcd文件 */ 
     pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-
     if (file_is_pcd)        // 加载pcd文件
     {
         if (pcl::io::loadPCDFile(argv[filenames[0]], *source_cloud) < 0) 
@@ -73,63 +69,53 @@ int main(int argc, char **argv)
         }
     }
 
-    /* Reminder: how transformation matrices work :
-
+    /* 4. 使用两种方法操作点云坐标变换
+    Reminder: how transformation matrices work :
            |-------> This column is the translation
     | 1 0 0 x |  \
     | 0 1 0 y |   }-> The identity 3x3 matrix (no rotation) on the left
     | 0 0 1 z |  /
     | 0 0 0 1 |    -> We do not use this line (and it has to stay 0,0,0,1)
 
-    METHOD #1: Using a Matrix4f ====================================第二种方法
-    This is the "manual" method, perfect to understand but error prone !
-  */
-    // 创建4x4单位阵
-    Eigen::Matrix4f transform_1 = Eigen::Matrix4f::Identity();
+    METHOD #1: Using a Matrix4f ====================================第一种方法
+        This is the "manual" method, perfect to understand but error prone !
+    METHOD #2: Using a Affine3f ====================================第二种方法
+        This method is easier and less error prone！
+    */
 
-    // Define a rotation matrix (see https://en.wikipedia.org/wiki/Rotation_matrix)
+    /* 4.1 通过4*4齐次变换矩阵操作点云坐标变换 */
+    Eigen::Matrix4f transform_1 = Eigen::Matrix4f::Identity();      // 先初始化为单位矩阵
+    // 定义旋转矩阵
     float theta = M_PI / 4; // The angle of rotation in radians
-    transform_1(0, 0) = std::cos(theta);
-    transform_1(0, 1) = -sin(theta);
-    transform_1(1, 0) = sin(theta);
-    transform_1(1, 1) = std::cos(theta);
-    //    (row, column)
+    transform_1(0, 0) = std::cos(theta);    // 根据旋转角度，填充旋转矩阵(0, 0)
+    transform_1(0, 1) = -sin(theta);        // 根据旋转角度，填充旋转矩阵(0, 1)
+    transform_1(1, 0) = sin(theta);         // 根据旋转角度，填充旋转矩阵(1, 0)
+    transform_1(1, 1) = std::cos(theta);    // 根据旋转角度，填充旋转矩阵(1, 1)
+    // 定义平移向量
+    transform_1(0, 3) = 2.5;                // 根据x平移距离，填充平移向量(0, 3)
+    // 打印齐次变换矩阵
+    printf("Method #1: using a Matrix4f\n");    
+    std::cout << transform_1 << std::endl;      // 打印齐次变换矩阵
 
-    // Define a translation of 2.5 meters on the x axis.
-    transform_1(0, 3) = 2.5;
-
-    // Print the transformation
-    printf("Method #1: using a Matrix4f\n");
-    std::cout << transform_1 << std::endl;
-
-    /*  METHOD #2: Using a Affine3f ====================================第二种方法
-    This method is easier and less error prone
-  */
+    /* 4.2 通过Eigen::Affine3f操作点云坐标变换  */ 
     Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
-
-    // Define a translation of 2.5 meters on the x axis.
-    transform_2.translation() << 2.5, 0.0, 0.0;
-
-    // The same rotation matrix as before; theta radians around Z axis
-    transform_2.rotate(Eigen::AngleAxisf(theta, Eigen::Vector3f::UnitZ()));
-
-    // Print the transformation
+    transform_2.translation() << 2.5, 0.0, 0.0;                                 // 定义平移向量
+    transform_2.rotate(Eigen::AngleAxisf(theta, Eigen::Vector3f::UnitZ()));     // 定义绕Z轴的旋转
+    // 打印齐次变换矩阵
     printf("\nMethod #2: using an Affine3f\n");
-    std::cout << transform_2.matrix() << std::endl;
+    std::cout << transform_2.matrix() << std::endl;     // 打印变换矩阵。transform_1=transform_2.matrix()
 
-    // Executing the transformation
+    /* 4.3 执行坐标变换 */
     pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-    // You can either apply transform_1 or transform_2; they are the same
-    pcl::transformPointCloud(*source_cloud, *transformed_cloud, transform_2);
+    pcl::transformPointCloud(*source_cloud, *transformed_cloud, transform_2);  // Transform_1 和 transform_2 一样
 
-    // Visualization  可视化
+    /* 5. 点云可视化 */
     printf("\nPoint cloud colors :  white  = original point cloud\n"
            "                        red  = transformed point cloud\n");
     pcl::visualization::PCLVisualizer viewer("Matrix transformation example");
 
     // Define R,G,B colors for the point cloud
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler(source_cloud, 255, 255, 255);
-    // We add the point cloud to the viewer and pass the color handler
     viewer.addPointCloud(source_cloud, source_cloud_color_handler, "original_cloud");
 
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler(transformed_cloud, 230, 20, 20); // Red
--- 01base/2matrix_transform/z01-2-note.md
+++ 01base/2matrix_transform/z01-2-note.md
@@ -1,27 +1,34 @@
 # 点云矩阵变换
 
 ## 1. 做了个“-h”去show help
-使用的PCL函数：
-- `pcl::console::find_switch`：pcl中用来检测命令行参数的函数，主要用来判断命令行参数列表中是否包含指定的标志。
-
-其他函数：
-- 代码中`program_name`可以获取当前程序名称。
+1. 使用的PCL函数：
+   - `pcl::console::find_switch`：pcl中用来检测命令行参数的函数，主要用来判断命令行参数列表中是否包含指定的标志，返回bool值。
+   其他函数：
+   - 代码中`program_name`可以获取当前程序名称。
 
 
 ## 2. 从命令行参数中提取一个.ply或者.pcd文件名称
-
-
+1. 使用的PCL函数：
+   - `pcl::console::parse_file_extension_argument`：pcl中解析命令行参数，以获取具有给定扩展名向量的文件名，返回包含文件名索引的向量。
 
 
 ## 3. 加载ply/pcd文件
+1. 使用的PCL函数：
+   - `pcl::io::loadPCDFile`：pcl中根据文件名加载pcd点云的函数。
+   - `pcl::io::loadPLYFile`：pcl中根据文件名加载ply点云的函数。
 
 
+## 4. 使用两种方式操作点云坐标变换
+### 4.1 使用Eigen::Matrix4f操作点云坐标变换
+Eigen::Matrix4f声明一个齐次变换矩阵，手动计算旋转矩阵和平移向量，对该齐次变换矩阵进行填充，得到想要的齐次变换矩阵。
 
-## 4.1 使用Eigen::Matrix4f操作点云旋转
+## 4.2 使用Eigen::Affine3f操作点云坐标变换
+Eigen::Affine3f声明一个仿射变换类，指定其平移向量和旋转矩阵，不需要写公式计算，更加直观，得到的结果和Eigen::Matrix4f一样。通过`transform_2.matrix()`可以转换为`transform1`。
 
+## 4.3 执行坐标变换
+1. 使用的PCL函数：
+   - `pcl::transformPointCloud`：对包含类型为PointXY的点的点云应用仿射变换。
 
-
-## 4.2 使用Eigen::Affine3f操作点云旋转
-
-
+## 5 可视化
+-
 
